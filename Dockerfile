64f3a28a85eeae7c64c89ff8b0af8aa803727ea3
Yfiua <yfiua6@gmail.com>
2024-08-03 19:37:42 +0200

    add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# gdelt-downloader
+# Version: 0.1
+# Description: Dockerfile for gdelt-downloader
+# Author: github.com/yfiua
+
+# Use the official Python base image
+FROM python:3.9-slim
+
+# Variables
+ENV n_jobs 1
+ENV start_date 19700101
+ENV end_date 99991231
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Update and Install packages
+RUN apt-get update -y \
+        && apt-get install -y \
+        wget \
+        unzip
+
+# Add stu
+ADD https://github.com/yfiua/stu/releases/download/2.7.82/stu_amd64.deb stu_amd64.deb
+RUN dpkg -i stu_amd64.deb
+
+# Ensure the script is executable
+RUN chmod +x /app/*.py
+
+# Run the script by default when the container starts
+CMD [ "sh", "-c", "echo ${start_date} > START_DATE && echo ${end_date} > END_DATE && stu -j ${n_jobs}" ]
---
+++ mkfilenames.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+import argparse
+from datetime import datetime
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Filter the URLs with dates.')
+parser.add_argument('--start_date', type=str, help='Date in YYYYMMDD format to filter the data.')
+parser.add_argument('--end_date', type=str, help='Date in YYYYMMDD format to filter the data.')
+args = parser.parse_args()
+
+# Convert the date strings to datetime objects
+if args.start_date:
+    try:
+        start_date = datetime.strptime(args.start_date, '%Y%m%d')
+    except ValueError:
+        print("Error: Invalid start_date format. Use YYYYMMDD format.")
+        sys.exit(1)
+else:
+    start_date = datetime.strptime('19700101', '%Y%m%d')
+
+if args.end_date:
+    try:
+        end_date = datetime.strptime(args.end_date, '%Y%m%d')
+    except ValueError:
+        print("Error: Invalid end_date format. Use YYYYMMDD format.")
+        sys.exit(1)
+else:
+    end_date = datetime.strptime('99991231', '%Y%m%d')
+
+# Check if start_date < end_date
+if args.start_date and args.end_date:
+    if args.start_date > args.end_date:
+        print("Error: start_date should be less than end_date.")
+        sys.exit(1)
+
+# Set UTF-8 encoding for stdout
+sys.stdout.reconfigure(encoding='utf-8')
+
+# Regex pattern to match the URL and extract the filename
+pattern = re.compile(r'^\s*\S+\s+\S+\s*\S+gdeltv2/(\S+)\.zip$')
+pattern_filename = re.compile(r'(\d{14})\.(\S+)\.\S+$')
+
+# Read from standard input
+for line in sys.stdin:
+    line = line.strip()
+
+    # Match the pattern
+    match = pattern.match(line)
+    if match:
+        match_filename = pattern_filename.match(match.group(1))
+
+        if match_filename:
+            if end_date < datetime.strptime(match_filename.group(1)[:8], '%Y%m%d'):
+                sys.exit(0)
+
+            # Print the extracted filename prefixed with "data/"
+            if start_date <= datetime.strptime(match_filename.group(1)[:8], '%Y%m%d'):
+                print(f"data/{match_filename.group(2)}/{match.group(1)}")
+
